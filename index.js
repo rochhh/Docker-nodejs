2821c601e41b79471215a78f827c400c35cd0c07
Rochishnu Paliwal <rochishnu7@gmail.com>
2022-08-15 12:44:31 +0530

    git checking test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/" , (req,res)=>{
-    res.send("<h1>Django!!!! Angular is the best !!!! Django!!!! </h1>")
+    res.send("<h1>Django!!!! Angular is the best broooooo !!!! Django is also chill !!!! </h1>")
 })
 
 app.listen( port , ()=>{ console.log(`listening on port -> ${port} `) })
